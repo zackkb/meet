afc3fcf1479cf8c0b9fc0d9812392d3a51913461
zackkb <98679473+zackkb@users.noreply.github.com>
2022-07-09 10:42:48 -0500

    atatus

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
-import * as atatus from "atatus-spa";
 
+import * as atatus from "atatus-spa";
 atatus.config("2fbbfec7c75d441bb4f41de96eac3a41").install();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
