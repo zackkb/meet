854fbd05ef2d8e76e66668f7056365abefa0047e
zackkb <98679473+zackkb@users.noreply.github.com>
2022-07-09 23:54:56 -0500

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import * as atatus from "atatus-spa";
-atatus.config("2f3beb4839df4dba88c06fbcbcda6c02").install();
+atatus.config("60f4a3d59fbe4afe9f2c6b1466b03412").install();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
